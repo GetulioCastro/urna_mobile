a0519b1945cd0def12f47ec2f7601d77adae1d14
GetulioCastro <augustoge71@gmail.com>
2021-03-18 19:33:58 -0300

    Corrigida urls.py

--- urnamobile/urls.py
+++ urnamobile/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from urnamobile.eleitor import eleitor
+
 
 urlpatterns = [
-    path('eleitor/', include('eleitor.urls')),
     path('admin/', admin.site.urls),
+    path('', eleitor),
 ]
