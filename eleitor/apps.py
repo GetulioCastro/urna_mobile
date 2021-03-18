2f8a81ede4fc3b15eaadc42d2d247d8451801c88
GetulioCastro <augustoge71@gmail.com>
2021-03-18 17:26:38 -0300

    Emendar as alterações

--- eleitor/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EleitorConfig(AppConfig):
-    name = 'eleitor'
--- urnamobile/eleitor/views.py
+++ urnamobile/eleitor/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
 def eleitor(request):
-    return HttpResponse("Hello, World! Vocẽ é o eleitor pioneiro neste aplicativo.")
-    
+    return HttpResponse("Olá, mundo! Vocẽ é o eleitor pioneiro neste aplicativo.")
+
