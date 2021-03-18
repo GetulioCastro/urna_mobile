bdd9c1a0f91c08eec0da714135ec61d183bac68e
GetulioCastro <augustoge71@gmail.com>
2021-03-18 19:11:36 -0300

    Corrigida "views.py" conforme flake8

--- eleitor/views.py
+++ eleitor/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 # Create your views here.
 
+
 def eleitor(request):
     return HttpResponse("Hello, World! Vocẽ é o eleitor pioneiro neste aplicativo.")
-    
+
